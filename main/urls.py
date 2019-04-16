5c6d12e078b12b035f450f933d86d16240cd36df
David Montes <david.montes@sjsu.edu>
2019-04-15 23:24:39 -0700

    updated project to match other

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='main-home'),
+	path('<int:id>', views.home, name='main-home'),
 	path('about/', views.about, name='main-about'),
-	path('map/', views.map, name='main-map'),
-	path('graphs/', views.graphs, name='main-graphs'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from requests.exceptions import ConnectionError
+import requests
 
-def home(request):
-	return render(request, 'main/home.html')
+def home(request, id=''):
+
+	#TODO
+	# if id is set, then need to get information about farm
+	# GetFarmsByUser(Joe123)
+	usersFarms = [1, 2, 3]
+
+	#url = 'http://13.57.209.41:8080/sensors'
+
+	#r = None
+	context = None
+
+	try:
+		#r = requests.get(url, timeout=0.001)
+		context = {
+			#'info': r.json(),
+			#'data': r.json()[0]['data'][0],
+			'id': id,
+			'usersFarms': usersFarms,
+		}
+	except ConnectionError as e:    # This is the correct syntax
+#		print(e)
+		context = {
+			'error': "Hmmm, our server appears to be taking a long time to respond.  Please try again in a few minutes."
+		}
+
+	return render(request, 'main/home.html', context)
 
 def about(request):
 	return render(request, 'main/about.html')
-
-def map(request):
-	return render(request, 'main/map.html')
-
-def graphs(request):
-	return render(request, 'main/graphs.html')
